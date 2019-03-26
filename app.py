dba139529d749a618adf1f4142eb374a59692c96
Chong Sun <sonshicyu@gmail.com>
2019-03-25 23:27:40 -0500

    Revise the requirement.txt

--- app.py
+++ app.py
@@ -51,5 +51,6 @@ def graph():
     return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+#    port = int(os.environ.get("PORT", 5000))
+#    app.run(host='0.0.0.0', port=port)
+    app.run(port=33507)
